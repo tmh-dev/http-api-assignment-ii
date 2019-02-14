3d107e39d0e9c0b4c3397a2d42165b123d2c058e
Tyler Hernandez <tmh6112@rit.edu>
2019-02-13 21:49:43 -0500

    mostly done

---
+++ src/responses.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const style = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const users = {};
+
+const respond = (request, response, status, type, object) => {
+  const headers = {
+    'Content-Type': type,
+  };
+
+  response.writeHead(status, headers);
+  response.write(object);
+  response.end();
+};
+
+const respondMeta = (request, response, status, type) => {
+  response.writeHead(status, { 'Content-Type': type });
+};
+
+const getIndex = (request, response) => {
+  respond(request, response, 200, 'text/html', index);
+};
+
+const getCSS = (request, response) => {
+  respond(request, response, 200, 'text/css', style);
+};
+
+// function for 404 not found requests with message
+const notReal = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    id: 'notFound',
+  };
+
+  if (request.method === 'GET') responseJSON.message = 'The page you are looking for was not found.';
+
+  // return a 404 with an error message
+  respond(request, response, 404, 'application/json', JSON.stringify(responseJSON));
+};
+
+const notRealMeta = (request, response) => {
+  respondMeta(request, response, 404, 'application/json');
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  respond(request, response, 200, 'application/json', JSON.stringify(responseJSON));
+};
+
+const getUsersMeta = (request, response) => respondMeta(request, response, 200, 'application/json');
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respond(request, response, 400, 'application/json', JSON.stringify(responseJSON));
+  }
+
+  let responseCode = 201;
+
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respond(request, response, responseCode, 'application/json', JSON.stringify(responseJSON));
+  }
+
+  return respondMeta(request, response, responseCode, 'application/json');
+};
+
+
+module.exports = {
+  getIndex,
+  getCSS,
+  notReal,
+  getUsers,
+  addUser,
+  getUsersMeta,
+  notRealMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const responseHandler = require('./responses.js');
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
+    const body = [];
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+      responseHandler.addUser(request, response, bodyParams);
+    });
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/') responseHandler.getIndex(request, response);
+  else if (parsedUrl.pathname === '/style.css') responseHandler.getCSS(request, response);
+  else if (parsedUrl.pathname === '/getUsers') responseHandler.getUsers(request, response);
+  else if (parsedUrl.pathname === '/notReal') responseHandler.notReal(request, response);
+};
+
+const handleHead = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/getUsers') responseHandler.getUsersMeta(request, response);
+  else if (parsedUrl.pathname === '/notReal') responseHandler.notRealMeta(request, response);
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') handlePost(request, response, parsedUrl);
+  else if (request.method === 'HEAD') handleHead(request, response, parsedUrl);
+  else if (request.method === 'GET') handleGet(request, response, parsedUrl);
+};
+
+const app = http.createServer(onRequest);
+
+app.listen(PORT);
+
+console.log(`Listening on 127.0.0.1: ${PORT}`);
